3f1f1e5ce712dbd9afb0c6a056ad486efedd577d
iliass-laa <iliasslaasri1999@gmail.com>
2025-12-26 17:41:08 +0000

    separation of mandatory from bonus

---
+++ mandatory/srcs/requirements/mariadb/tools/mdb-conf.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+service mariadb start
+sleep 3
+
+
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DB_N}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DB_N}\`.* TO \`${MYSQL_USER}\`@'%';"
+mysql -e "FLUSH PRIVILEGES;"
+
+mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
+
+exec "$@"
+
+
